fdbbf0bb41501b7e590fbe2e6d81e45d328d3360
Deltora <deltora1st@gmail.com>
2024-04-30 23:30:15 +0700

    update create schema

--- src/utils/query/create.js
+++ src/utils/query/create.js
@@ -2,6 +2,21 @@ export default {
   system: {
     createDb: 'CREATE DATABASE "',
     createSchema: `
+    SET statement_timeout = 0;
+    SET lock_timeout = 0;
+    SET idle_in_transaction_session_timeout = 0;
+    SET client_encoding = 'UTF8';
+    SET standard_conforming_strings = on;
+    SELECT pg_catalog.set_config('search_path', '', false);
+    SET check_function_bodies = false;
+    SET xmloption = content;
+    SET client_min_messages = warning;
+    SET row_security = off;
+    
+    SET default_tablespace = '';
+    
+    SET default_table_access_method = heap;
+    
     CREATE TABLE public."Dept" (
         "DeptID" uuid NOT NULL,
         "Name" character varying NOT NULL,
